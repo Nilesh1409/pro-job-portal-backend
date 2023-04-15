859a901d10b13b66343d0a174f97228912324f54
Nilesh Tiwari <nilesh@Nileshs-MacBook-Pro.local>
2023-04-15 16:57:34 +0530

    change mongooes version

--- src/services/mongoose.js
+++ src/services/mongoose.js
@@ -1,17 +1,17 @@
-'use strict'
+"use strict";
 
-const config = require('../config')
-const mongoose = require('mongoose')
+const config = require("../config");
+const mongoose = require("mongoose");
 // mongoose.Promise = require('bluebird')
 
-mongoose.connection.once('connected', () => {
-  console.log('MongoDB is connected')
-})
+mongoose.connection.once("connected", () => {
+  console.log("MongoDB is connected");
+});
 
-mongoose.connection.on('error', (err) => {
-  console.log(`Could not connect to MongoDB because of ${err}`)
-  process.exit(-1)
-})
+mongoose.connection.on("error", (err) => {
+  console.log(`Could not connect to MongoDB because of ${err}`);
+  process.exit(-1);
+});
 
 // if (config.env === 'dev') {
 //   mongoose.set('debug', true)
@@ -20,12 +20,17 @@ mongoose.connection.on('error', (err) => {
 // mongoose.set('useFindAndModify', false);
 // mongoose.set('useCreateIndex', true);
 exports.connect = () => {
-  var mongoURI = config.mongo.uri
+  var mongoURI = config.mongo.uri;
 
-  mongoose.connect(mongoURI, {dbName: 'proconsultant',useCreateIndex: true }, {
-    keepAlive: 1,
-    useNewUrlParser: true
-  })
+  mongoose.connect(
+    mongoURI,
+    { dbName: "proconsultant", useCreateIndex: true },
+    {
+      keepAlive: 1,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
 
-  return mongoose.connection
-}
+  return mongoose.connection;
+};
